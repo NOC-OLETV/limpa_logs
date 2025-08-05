a94f3baa1b44058a1b15ed3b06754ef2be07e745
NOC-OLETV <github@oletv.net.br>
2025-08-04 21:47:03 -0300

    Update install_limpa_logs.sh

--- install_limpa_logs.sh
+++ install_limpa_logs.sh
@@ -2,6 +2,6 @@
 #Dando permissão de execução
 chmod +x /usr/local/src/limpa_logs/limpa_logs.sh
 #Adicionando crontab
-(crontab -l 2>/dev/null; echo "*/1 * * * * /usr/local/src/limpa_logs/limpa_logs.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 3 * * * /usr/local/src/limpa_logs/limpa_logs.sh") | crontab -
 echo 'Scrip criado com sucesso'
 exit 0
