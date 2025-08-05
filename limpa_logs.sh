ba015e248b48ac3fef6dc0cc99b3e2f540cf7ce8
NOC-OLETV <github@oletv.net.br>
2025-08-04 21:44:35 -0300

    Update limpa_logs.sh

--- limpa_logs.sh
+++ limpa_logs.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#Novo continer
 docker exec  -w / --user root dockerole-proxy-1 bash -c 'rm -f /dev/shm/cache_live/* ; > /usr/share/nginx/logs/vai.log ; > /usr/share/nginx/logs/vai_hls.log'
+#Antigo conteiner
+#docker exec  -w / --user root docker_install_ole-proxy-1 bash -c 'rm -f /dev/shm/cache_live/* ; > /usr/share/nginx/logs/vai.log ; > /usr/share/nginx/logs/vai_hls.log'
